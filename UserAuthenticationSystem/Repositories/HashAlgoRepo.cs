5b756ba47735aa5ba3b046c51098a9e5e80f5594
Nada-Mamdouh <nada-mamdouh@outlook.com>
2023-07-27 01:17:42 +0300

    Created User Sign-up feature & password hashing & adding salt

---
+++ UserAuthenticationSystem/Repositories/HashAlgoRepo.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserAuthenticationSystem.Models;
+
+namespace UserAuthenticationSystem.Repositories
+{
+    public class HashAlgoRepo : IHashAlgoRepo
+    {
+        UserAuthenticationSystemDbContext _dbContext;
+        public HashAlgoRepo(UserAuthenticationSystemDbContext dbContext) {
+            _dbContext = dbContext;
+        }
+
+        public int AddHashAlgo(string algorithmname)
+        {
+            HashingAlgorithm ha = GetAlgorithmByName(algorithmname);
+            if (ha == null)
+            {
+                try
+                {
+                    ha = new HashingAlgorithm();
+                    ha.AlgorithmName = algorithmname;
+                    _dbContext.Add(ha);
+                    _dbContext.SaveChanges();
+                    return ha.HashAlgorithmId;
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"exeption while saving: {e.Message}");
+                }
+            }
+            return ha.HashAlgorithmId;
+        }
+
+        public HashingAlgorithm GetAlgorithmByName(string algoname)
+        {
+            HashingAlgorithm ha = _dbContext.HashingAlgorithms.FirstOrDefault(hashalgo => hashalgo.AlgorithmName == algoname);
+            return ha;
+        }
+    }
+}
---
+++ UserAuthenticationSystem/Repositories/IHashAlgoRepo.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using UserAuthenticationSystem.Models;
+
+namespace UserAuthenticationSystem.Repositories
+{
+    public interface IHashAlgoRepo
+    {
+        public int AddHashAlgo(string algorithmname);
+        public HashingAlgorithm GetAlgorithmByName(string algoname);
+    }
+}
