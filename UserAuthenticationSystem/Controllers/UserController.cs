7d088f423acbce3808cfbafb9758859541119419
Nada-Mamdouh <nada-mamdouh@outlook.com>
2023-07-29 21:15:38 +0300

    updating controller

--- UserAuthenticationSystem/Controllers/UserController.cs
+++ UserAuthenticationSystem/Controllers/UserController.cs
@@ -38,6 +38,16 @@ namespace UserAuthenticationSystem.Controllers
         [Route("Login")]
         public IActionResult Login(UserLoginDataViewModel users)
         {
+            /*** Today's work 29/7: implemented login function partially;
+             * could use jwt to actually generate token for user when trying to login 
+             */
+            /** TODO: For next session
+             * 1- Check if the user couldn't login then redirect to Register action: 
+             * 2- While generating jwt token, I need to verify password hash which means I need to hash incoming password and compare 
+             * it to the hash already exists in db, there's already verifypassord function inside "HashPasswordService" that do just that.
+             * 3- Complete login feature.
+             * 4- I need to work on that when user register for first time send confirmation token to his mail, to check his mail validity.
+             */
             var token = _jwtTokenServices.Authenticate(users);
 
             if (token == null)
